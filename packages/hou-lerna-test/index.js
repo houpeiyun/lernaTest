2ff9b86aa2d7d28e4a9294e937f59fea597b0564
houpeiyun <houpeiyun@58.com>
2021-12-17 10:28:24 +0800

    perf:lerna

--- packages/hou-lerna-test/index.js
+++ packages/hou-lerna-test/index.js
@@ -2,6 +2,6 @@ const _  = require('lodash');
 console.log('hou-lerna-test:',_.VERSION)
 module.exports = {
     test () {
-      console.log('~~~~hou-lerna-test~~~~');
+      console.log('~~~~hou-lerna-test');
     }
 }
